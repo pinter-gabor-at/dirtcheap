d86c0c54ec5ff57ba6efd0f99e236f1dafe891ca
Pintér Gábor <pinter.gabor@gmx.at>
2024-11-18 18:50:57 +0100

    1.0.4+mc1.21.2

--- src/main/java/eu/pintergabor/dirtcheap/datagen/ModRecipeGenerator.java
+++ src/main/java/eu/pintergabor/dirtcheap/datagen/ModRecipeGenerator.java
@@ -4,12 +4,11 @@ import eu.pintergabor.dirtcheap.Global;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.RecipeGenerator;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -24,18 +23,27 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
      * Generate recipes
      */
     @Override
-    public void generate(RecipeExporter exporter) {
-        // Generate two shapeless recipes:
-        // Mix DIRT with BONE_MEAL or with ROTTEN_FLESH to create 2 DIRTs.
-        for (Item i : new Item[]{Items.BONE_MEAL, Items.ROTTEN_FLESH}) {
-            ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.DIRT, 2)
-                    .input(i)
-                    .input(Items.DIRT)
-                    .criterion(hasItem(Items.DIRT), conditionsFromItem(Items.DIRT))
-                    .criterion(hasItem(i), conditionsFromItem(i))
-                    .offerTo(exporter, Identifier.of(
-                            Global.MODID,
-                            getRecipeName(Items.DIRT) + "-" + getRecipeName(i)));
-        }
+    protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registries, RecipeExporter exporter) {
+        return new RecipeGenerator(registries, exporter) {
+            @Override
+            public void generate() {
+                // Generate two shapeless recipes:
+                // Mix DIRT with BONE_MEAL or with ROTTEN_FLESH to create 2 DIRTs.
+                for (Item i : new Item[]{Items.BONE_MEAL, Items.ROTTEN_FLESH}) {
+                    createShapeless(RecipeCategory.BUILDING_BLOCKS, Items.DIRT, 2)
+                            .input(i)
+                            .input(Items.DIRT)
+                            .criterion(hasItem(Items.DIRT), conditionsFromItem(Items.DIRT))
+                            .criterion(hasItem(i), conditionsFromItem(i))
+                            .offerTo(exporter,
+                                    Global.MODID + ":" + getRecipeName(Items.DIRT) + "-" + getRecipeName(i));
+                }
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return Global.MODID + " recipes";
     }
 }
