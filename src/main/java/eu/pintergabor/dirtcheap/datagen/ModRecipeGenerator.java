b9f9ddd1d203a48dcb5e8a115a115d402bde8120
Pintér Gábor <pinter.gabor@gmx.at>
2024-11-18 17:00:32 +0100

    1.0.4+mc1.20.5

--- src/main/java/eu/pintergabor/dirtcheap/datagen/ModRecipeGenerator.java
+++ src/main/java/eu/pintergabor/dirtcheap/datagen/ModRecipeGenerator.java
@@ -7,27 +7,32 @@ import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.util.Identifier;
 
-public class ModRecipeGenerator extends FabricRecipeProvider {
-	public ModRecipeGenerator(FabricDataOutput output) {
-		super(output);
-	}
+import java.util.concurrent.CompletableFuture;
 
-	/**
-	 * Generate recipes
-	 */
-	@Override
-	public void generate(RecipeExporter exporter) {
-		// Generate two shapeless recipes:
-		// Mix DIRT with BONE_MEAL or with ROTTEN_FLESH to create 2 DIRTs.
-		for (Item i : new Item[]{Items.BONE_MEAL, Items.ROTTEN_FLESH}) {
-			ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.DIRT, 2)
-					.input(i)
-					.input(Items.DIRT)
-					.criterion(hasItem(Items.DIRT), conditionsFromItem(Items.DIRT))
-					.criterion(hasItem(i), conditionsFromItem(i))
-					.offerTo(exporter, new Identifier(getRecipeName(Items.DIRT) + "-" + getRecipeName(i)));
-		}
-	}
+public class ModRecipeGenerator extends FabricRecipeProvider {
+    public ModRecipeGenerator(
+            FabricDataOutput output,
+            CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    /**
+     * Generate recipes
+     */
+    @Override
+    public void generate(RecipeExporter exporter) {
+        // Generate two shapeless recipes:
+        // Mix DIRT with BONE_MEAL or with ROTTEN_FLESH to create 2 DIRTs.
+        for (Item i : new Item[]{Items.BONE_MEAL, Items.ROTTEN_FLESH}) {
+            ShapelessRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, Items.DIRT, 2)
+                    .input(i)
+                    .input(Items.DIRT)
+                    .criterion(hasItem(Items.DIRT), conditionsFromItem(Items.DIRT))
+                    .criterion(hasItem(i), conditionsFromItem(i))
+                    .offerTo(exporter, new Identifier(getRecipeName(Items.DIRT) + "-" + getRecipeName(i)));
+        }
+    }
 }
