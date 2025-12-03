2597e90c3785c650e29e185ecd7d79fe91640766
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-03 17:02:03 +0100

    Correct version number.

--- src/main/java/eu/pintergabor/dirtcheap/DataGen.java
+++ src/main/java/eu/pintergabor/dirtcheap/DataGen.java
@@ -1,16 +1,10 @@
 package eu.pintergabor.dirtcheap;
 
-import java.util.concurrent.CompletableFuture;
-
 import eu.pintergabor.dirtcheap.datagen.ModRecipeRunner;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
-
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 
 
 @EventBusSubscriber(modid = Global.MODID, value = Dist.CLIENT)
@@ -19,9 +13,6 @@ public class DataGen {
 	@SubscribeEvent
 	public static void init(GatherDataEvent.Client event) {
 		// Create recipes.
-		final DataGenerator generator = event.getGenerator();
-		final PackOutput output = generator.getPackOutput();
-		final CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-		event.addProvider(new ModRecipeRunner(output, lookupProvider));
+		event.createProvider(ModRecipeRunner::new);
 	}
 }
