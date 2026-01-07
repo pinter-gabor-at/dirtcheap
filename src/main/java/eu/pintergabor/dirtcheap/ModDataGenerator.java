38162693bcf399abfe690c47315f2c253a3b9740
Pintér Gábor <pinter.gabor@gmx.at>
2026-01-07 10:47:57 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/dirtcheap/ModDataGenerator.java
+++ src/main/java/eu/pintergabor/dirtcheap/ModDataGenerator.java
@@ -5,13 +5,14 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
+import org.jspecify.annotations.NonNull;
 
 
 @EventBusSubscriber(modid = Global.MODID, value = Dist.CLIENT)
-public class DataGen {
+public class ModDataGenerator {
 
 	@SubscribeEvent
-	public static void init(GatherDataEvent.Client event) {
+	public static void init(GatherDataEvent.@NonNull Client event) {
 		// Create recipes.
 		event.createProvider(ModRecipeRunner::new);
 	}
